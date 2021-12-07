6ebcb91f35cebc66cde0cc52c9f03ba9de29eb51
seanq9 <54922943+seanq9@users.noreply.github.com>
2021-12-07 14:39:34 -0500

    Playing with webcamera

--- BGRDetector.py
+++ BGRDetector.py
@@ -1,6 +1,6 @@
 '''
-Code Acquired from:
-https://maker.pro/raspberry-pi/tutorial/using-opencv-and-raspberry-pi-to-visualize-images-in-different-color-spaces
+Acquired from: https://www.geeksforgeeks.org/detect-the-rgb-color-from-a-webcam-using-python-opencv/
+and: https://maker.pro/raspberry-pi/tutorial/using-opencv-and-raspberry-pi-to-visualize-images-in-different-color-spaces
 '''
 
 import cv2
@@ -8,68 +8,69 @@ import numpy as np
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 
+class Camera:
+    def __init__(self):
+        # taking the input from webcam
+        #vid = cv2.VideoCapture(0)
+        # from pi
+        self.camera = PiCamera()
+        self.camera.resolution = (640, 480)
+        self.camera.framerate = 30
+        self.rawCapture = PiRGBArray(camera, size=(640,480))
+        pass
 
-def nothing(x):
-    pass
+    def get_judgement(self):
+        # running while loop just to make sure that
+        # our program keep running untill we stop it
+        #while True:
+        #for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        self.camera.capture(self.rawCapture, format="bgr", use_video_port=True):
+        # capturing the current frame
+        #_, frame = vid.read()
 
+        # displaying the current frame
+        cv2.imshow("frame", self.rawCapture)
 
-im = np.zeros((300, 512, 3), np.uint8)
-cv2.namedWindow("Trackbars")
+        # setting values for base colors
+        b = self.rawCapture[:, :, :1]
+        g = self.rawCapture[:, :, 1:2]
+        r = self.rawCapture[:, :, 2:]
 
-cv2.createTrackbar("B", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("G", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("R", "Trackbars", 0, 255, nothing)
+        # computing the mean
+        b_mean = np.mean(b)
+        g_mean = np.mean(g)
+        r_mean = np.mean(r)
 
-lowerBound = np.array([33, 80, 40])
-upperBound = np.array([102, 255, 255])
+        # displaying the most prominent color
+        if (r_mean > g_mean and r_mean > b_mean):
+            return "Red"
+        else:
+            return "White"
+        #result = input("Type White or Red: >")
+        #return result
 
-kernelOpen = np.ones((5, 5))
-kernelClose = np.ones((20, 20))
+def get_outcome():
+    camera = Camera()
+    judge = []
+    good = 0
+    no_good = 0
+    for i in range(3):
+        input("Press any key for next judgement")
+        judge.append(camera.get_judgement())
+        if judge[i] == "Red":
+            no_good += 1
+        elif judge[i] == "White":
+            good += 1
+    if good > no_good:
+        outcome = "Pass"
+    elif no_good > good:
+        outcome = "Fail"
 
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 30
+    return judge, outcome
 
-rawCapture = PiRGBArray(camera, size=(640, 480))
-
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    img = frame.array
-
-    # Convert BGR to HSV
-    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    B = cv2.getTrackbarPos("B", "Trackbars")
-    G = cv2.getTrackbarPos("G", "Trackbars")
-    R = cv2.getTrackbarPos("R", "Trackbars")
-
-    im[:] = [B, G, R]
-
-    green = np.uint8([[[B, G, R]]])
-    hsvGreen = cv2.cvtColor(green, cv2.COLOR_BGR2HSV)
-    lowerBound = np.uint8([hsvGreen[0][0][0] - 10, 100, 100])
-    upperBound = np.uint8([hsvGreen[0][0][0] + 10, 255, 255])
-
-    # Create the Mask
-    mask = cv2.inRange(imgHSV, lowerBound, upperBound)
-
-    # Morphology
-    maskOpen = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernelOpen)
-    maskClose = cv2.morphologyEx(maskOpen, cv2.MORPH_CLOSE, kernelClose)
-
-    maskFinal = maskClose
-    _, conts, h = cv2.findContours(maskFinal.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    for i in range(len(conts)):
-        x, y, w, h = cv2.boundingRect(conts[i])
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-    cv2.imshow("mask", mask)
-    cv2.imshow("cam", img)
-    cv2.imshow("Trackbars", im)
-
-    key = cv2.waitKey(1)
-    rawCapture.truncate(0)
-    if key == 27:
-        break
-
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    judge, outcome = get_outcome()
+    print("Judge 1: " + judge[0])
+    print("Judge 2: " + judge[1])
+    print("Judge 3: " + judge[2])
+    print("Outcome: " + outcome)
