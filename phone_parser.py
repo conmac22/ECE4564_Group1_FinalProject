58f97a74a7aecffbfaf203a7fefd6a2c7d8a9490
seanq9 <54922943+seanq9@users.noreply.github.com>
2021-12-09 11:13:05 -0500

    Added competition name

--- phone_parser.py
+++ phone_parser.py
@@ -1,9 +1,10 @@
 '''
 Methods for parsing messages from a mobile phone
 Author: Sean Quiterio
-Date last modified: 6 December, 2021
+Date last modified: 9 December, 2021
 '''
 
+# Example competition message: "Competition: <Name>"
 # Example message: "Lifter: <Name>, Lift: <Lift>, Attempt: <number>"
 
 def parse_message(message):
@@ -13,3 +14,8 @@ def parse_message(message):
     lift = message_params[1].split(' ')[1]
     attempt = message_params[2].split(' ')[1]
     return lifter, lift, int(attempt)
+
+def parse_competition(message):
+    message = message.trim()
+    comp = message.spli(' ')
+    return comp[1]
